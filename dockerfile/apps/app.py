a9172eaf2de68e101aa3fffc4064ffd8f9c1ba30
Yajinyaya <923130308@qq.com>
2021-11-02 18:41:54 -0400

    Now the flask server takes in json input as well

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -11,15 +11,29 @@ def hello():
 
 @app.route('/predict')
 def predict():
-	 #use entries from the query string here but could also use json
-     age = request.args.get('age')
-     absences = request.args.get('absences')
-     health = request.args.get('health')
-     data = [[age],[health],[absences]]
-     query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
-     query = pd.get_dummies(query_df)
-     prediction = clf.predict(query)
-     return jsonify(np.asscalar(prediction))
+    #use entries from the query string here but could also use json
+    age, absences, health = "", "", ""
+    if request.is_json:
+        age = request.json.get('age', None)
+        absences = request.json.get('absences', None)
+        health = request.json.get('health', None)
+        if (not age or not absences or not health):
+            return jsonify({"msg": "Bad json input. Parameters are missing"}), 400
+    else:
+        age = request.args.get('age')
+        absences = request.args.get('absences')
+        health = request.args.get('health')
+        if (not age or not absences or not health):
+            if not request.is_json:
+                return jsonify({"msg": "Missing query string. ZZZ"}), 400
+
+       
+
+    data = [[age],[health],[absences]]
+    query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
+    query = pd.get_dummies(query_df)
+    prediction = clf.predict(query)
+    return jsonify(np.asscalar(prediction)), 200
 
 if __name__ == '__main__':
     clf = joblib.load('/apps/model.pkl')
