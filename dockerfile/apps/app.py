4830b3bdb05c98ec07093db4c04b92cce4a5165e
Yajinyaya <923130308@qq.com>
2021-11-02 21:08:38 -0400

    Added test cases for the microservice

--- dockerfile/apps/app.py
+++ dockerfile/apps/app.py
@@ -12,46 +12,40 @@ def hello():
 @app.route('/predict')
 def predict():
 
-    age, absences, health = "", "", ""
+    age, absences, health, studytime, goout, failures, famrel = "", "", "", "", "", "", ""
     if request.is_json:
-        age = request.json.get('age', None)
-        absences = request.json.get('absences', None)
-        health = request.json.get('health', None)
-        if (not age or not absences or not health):
+        age = request.json.get('age',None)
+        absences = request.json.get('absences',None)
+        health = request.json.get('health',None)
+        studytime = request.json.get('studytime',None)
+        goout = request.json.get('goout',None)
+        failures = request.json.get('failures',None)
+        famrel = request.json.get('famrel',None)
+        
+        if (not age or not absences or not health or not studytime or not goout or not failures or not famrel):
             return jsonify({"msg": "Bad json input. Parameters are missing"}), 400
     else:
         age = request.args.get('age')
         absences = request.args.get('absences')
         health = request.args.get('health')
-        if (not age or not absences or not health):
-            if not request.is_json:
-                return jsonify({"msg": "Missing query string. ZZZ"}), 400
+        studytime = request.args.get('studytime')
+        goout = request.args.get('goout')
+        failures = request.args.get('failures')
+        famrel = request.args.get('famrel')
+        if (not age or not absences or not health or not studytime or not goout or not failures or not famrel):
+        
+            return jsonify({"msg": "Missing query string."}), 400
 
-       
+  
+  
+ 
 
-    data = [[age],[health],[absences]]
-    query_df = pd.DataFrame({ 'age' : pd.Series(age) ,'health' : pd.Series(health) ,'absences' : pd.Series(absences)})
+    data = [[studytime],[absences],[age],[health],[goout],[failures],[famrel]]
+    query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime), 'absences' : pd.Series(absences), 'age': pd.Series(age), 'health': pd.Series(health), 
+                        'goout': pd.Series(goout), 'failures': pd.Series(failures), 'famrel': pd.Series(famrel)})
     query = pd.get_dummies(query_df)
     prediction = clf.predict(query)
-    return jsonify(np.asscalar(prediction)), 200
-
-
-  
-  
-     age = request.args.get('age')
-     absences = request.args.get('absences')
-     health = request.args.get('health')
-     studytime = request.args.get('studytime')
-     goout = request.args.get('goout')
-     failures = request.args.get('failures')
-     famrel = request.args.get('famrel')
-
-     data = [[studytime],[absences],[age],[health],[goout],[failures],[famrel]]
-     query_df = pd.DataFrame({ 'studytime' : pd.Series(studytime), 'absences' : pd.Series(absences), 'age': pd.Series(age), 'health': pd.Series(health), 
-                          'goout': pd.Series(goout), 'failures': pd.Series(failures), 'famrel': pd.Series(famrel)})
-     query = pd.get_dummies(query_df)
-     prediction = clf.predict(query)
-     return jsonify(np.asscalar(prediction))
+    return jsonify(np.asscalar(prediction))
 
 if __name__ == '__main__':
     clf = joblib.load('/apps/model.pkl')
---
+++ test_app.py
@@ -0,0 +1,24 @@
+import requests
+import pytest
+
+# may need to modify clf reference and model path in apps.py as well
+# may need to modify this path
+from dockerfile.apps.app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_client(client):
+    r = client.get('/')
+    print("\n====== Result TEST_CLIENT ======")
+    print(r.data)
+    print("TEST PASSED!")
+
+def test_predict(client):
+    r = client.get('/predict?age=15&health=5&absences=5&studytime=4&goout=5&failures=2&famrel=5')
+    print("\n====== Result TEST_PREDICT ======")
+    print(int(r.data))
+    assert(int(r.data) == 1)
+    print("TEST PASSED!")
